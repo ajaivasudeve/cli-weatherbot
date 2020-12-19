10fa398591d57ded2c1a41cf61adf40b9977c4ad
ajaivasudeve <ajaivasudeve@gmail.com>
2020-12-19 14:16:02 +0530

    weatherbot: add a library indexing ISO Alpha-2 country codes
    - add country_codes.h
    
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

---
+++ country_codes.h
@@ -0,0 +1,279 @@
+#define COUNTRY_INIT_A "Afghanistan                                : AF\n" \
+"Aland Islands                              : AX\n" \
+"Albania                                    : AL\n" \
+"Algeria                                    : DZ\n" \
+"American Samoa                             : AS\n" \
+"Andorra                                    : AD\n" \
+"Angola                                     : AO\n" \
+"Anguilla                                   : AI\n" \
+"Antarctica                                 : AQ\n" \
+"Antigua and Barbuda                        : AG\n" \
+"Argentina                                  : AR\n" \
+"Armenia                                    : AM\n" \
+"Aruba                                      : AW\n" \
+"Australia                                  : AU\n" \
+"Austria                                    : AT\n" \
+"Azerbaijan                                 : AZ\n"
+
+#define COUNTRY_INIT_B "Bahamas                                    : BS\n" \
+"Bahrain                                    : BH\n" \
+"Bangladesh                                 : BD\n" \
+"Barbados                                   : BB\n" \
+"Belarus                                    : BY\n" \
+"Belgium                                    : BE\n" \
+"Belize                                     : BZ\n" \
+"Benin                                      : BJ\n" \
+"Bermuda                                    : BM\n" \
+"Bhutan                                     : BT\n" \
+"Bolivia                                    : BO\n" \
+"Bonaire, Sint Eustatius and Saba           : BQ\n" \
+"Bosnia and Herzegovina                     : BA\n" \
+"Botswana                                   : BW\n" \
+"Bouvet Island                              : BV\n" \
+"Brazil                                     : BR\n" \
+"British Indian Ocean Territory             : IO\n" \
+"Brunei Darussalam                          : BN\n" \
+"Bulgaria                                   : BG\n" \
+"Burkina Faso                               : BF\n" \
+"Burundi                                    : BI\n"
+
+#define COUNTRY_INIT_C "Cambodia                                   : KH\n" \
+"Cameroon                                   : CM\n" \
+"Canada                                     : CA\n" \
+"Cape Verde                                 : CV\n" \
+"Cayman Islands                             : KY\n" \
+"Central African Republic                   : CF\n" \
+"Chad                                       : TD\n" \
+"Chile                                      : CL\n" \
+"China                                      : CN\n" \
+"Christmas Island                           : CX\n" \
+"Cocos (Keeling) Islands                    : CC\n" \
+"Colombia                                   : CO\n" \
+"Comoros                                    : KM\n" \
+"Congo                                      : CG\n" \
+"Congo, The Democratic Republic of          : CD\n" \
+"Cook Islands                               : CK\n" \
+"Costa Rica                                 : CR\n" \
+"Cote d'Ivoire                              : CI\n" \
+"Croatia                                    : HR\n" \
+"Cuba                                       : CU\n" \
+"Curaçao                                    : CW\n" \
+"Cyprus                                     : CY\n" \
+"Czechia                                    : CZ\n"
+
+#define COUNTRY_INIT_D "Denmark                                    : DK\n" \
+"Djibouti                                   : DJ\n" \
+"Dominica                                   : DM\n" \
+"Dominican Republic                         : DO\n"
+
+#define COUNTRY_INIT_E "Ecuador                                    : EC\n" \
+"Egypt                                      : EG\n" \
+"El Salvador                                : SV\n" \
+"Equatorial Guinea                          : GQ\n" \
+"Eritrea                                    : ER\n" \
+"Estonia                                    : EE\n" \
+"Ethiopia                                   : ET\n"
+
+#define COUNTRY_INIT_F "Falkland Islands (Malvinas)                : FK\n" \
+"Faroe Islands                              : FO\n" \
+"Fiji                                       : FJ\n" \
+"Finland                                    : FI\n" \
+"France                                     : FR\n" \
+"French Guiana                              : GF\n" \
+"French Polynesia                           : PF\n" \
+"French Southern Territories                : TF\n"
+
+#define COUNTRY_INIT_G "Gabon                                      : GA\n" \
+"Gambia                                     : GM\n" \
+"Georgia                                    : GE\n" \
+"Germany                                    : DE\n" \
+"Ghana                                      : GH\n" \
+"Gibraltar                                  : GI\n" \
+"Greece                                     : GR\n" \
+"Greenland                                  : GL\n" \
+"Grenada                                    : GD\n" \
+"Guadeloupe                                 : GP\n" \
+"Guam                                       : GU\n" \
+"Guatemala                                  : GT\n" \
+"Guernsey                                   : GG\n" \
+"Guinea                                     : GN\n" \
+"Guinea-Bissau                              : GW\n" \
+"Guyana                                     : GY\n"
+
+#define COUNTRY_INIT_H "Haiti                                      : HT\n" \
+"Heard and Mc Donald Islands                : HM\n" \
+"Holy See (Vatican City State)              : VA\n" \
+"Honduras                                   : HN\n" \
+"Hong Kong                                  : HK\n" \
+"Hungary                                    : HU\n"
+
+#define COUNTRY_INIT_I "Iceland                                    : IS\n" \
+"India                                      : IN\n" \
+"Indonesia                                  : ID\n" \
+"Iran, Islamic Republic of                  : IR\n" \
+"Iraq                                       : IQ\n" \
+"Ireland                                    : IE\n" \
+"Isle of Man                                : IM\n" \
+"Israel                                     : IL\n" \
+"Italy                                      : IT\n"
+
+#define COUNTRY_INIT_J "Jamaica                                    : JM\n" \
+"Japan                                      : JP\n" \
+"Jersey                                     : JE\n" \
+"Jordan                                     : JO\n"
+
+#define COUNTRY_INIT_K "Kazakstan                                  : KZ\n" \
+"Kenya                                      : KE\n" \
+"Kiribati                                   : KI\n" \
+"Korea, Democratic People's Republic of     : KP\n" \
+"Korea, Republic of                         : KR\n" \
+"Kosovo (temporary code)                    : XK\n" \
+"Kuwait                                     : KW\n" \
+"Kyrgyzstan                                 : KG\n"
+
+#define COUNTRY_INIT_L "Lao, People's Democratic Republic          : LA\n" \
+"Latvia                                     : LV\n" \
+"Lebanon                                    : LB\n" \
+"Lesotho                                    : LS\n" \
+"Liberia                                    : LR\n" \
+"Libyan Arab Jamahiriya                     : LY\n" \
+"Liechtenstein                              : LI\n" \
+"Lithuania                                  : LT\n" \
+"Luxembourg                                 : LU\n"
+
+#define COUNTRY_INIT_M "Macao                                      : MO\n" \
+"Macedonia, The Former Yugoslav Republic Of : MK\n" \
+"Madagascar                                 : MG\n" \
+"Malawi                                     : MW\n" \
+"Malaysia                                   : MY\n" \
+"Maldives                                   : MV\n" \
+"Mali                                       : ML\n" \
+"Malta                                      : MT\n" \
+"Marshall Islands                           : MH\n" \
+"Martinique                                 : MQ\n" \
+"Mauritania                                 : MR\n" \
+"Mauritius                                  : MU\n" \
+"Mayotte                                    : YT\n" \
+"Mexico                                     : MX\n" \
+"Micronesia, Federated States of            : FM\n" \
+"Moldova, Republic of                       : MD\n" \
+"Monaco                                     : MC\n" \
+"Mongolia                                   : MN\n" \
+"Montenegro                                 : ME\n" \
+"Montserrat                                 : MS\n" \
+"Morocco                                    : MA\n" \
+"Mozambique                                 : MZ\n" \
+"Myanmar                                    : MM\n"
+
+#define COUNTRY_INIT_N "Namibia                                    : NA\n" \
+"Nauru                                      : NR\n" \
+"Nepal                                      : NP\n" \
+"Netherlands                                : NL\n" \
+"Netherlands Antilles                       : AN\n" \
+"New Caledonia                              : NC\n" \
+"New Zealand                                : NZ\n" \
+"Nicaragua                                  : NI\n" \
+"Niger                                      : NE\n" \
+"Nigeria                                    : NG\n" \
+"Niue                                       : NU\n" \
+"Norfolk Island                             : NF\n" \
+"Northern Mariana Islands                   : MP\n" \
+"Norway                                     : NO\n"
+
+#define COUNTRY_INIT_O "Oman                                         : OM\n"
+
+#define COUNTRY_INIT_P "Pakistan                                   : PK\n" \
+"Palau                                      : PW\n" \
+"Palestinian Territory, Occupied            : PS\n" \
+"Panama                                     : PA\n" \
+"Papua New Guinea                           : PG\n" \
+"Paraguay                                   : PY\n" \
+"Peru                                       : PE\n" \
+"Philippines                                : PH\n" \
+"Pitcairn                                   : PN\n" \
+"Poland                                     : PL\n" \
+"Portugal                                   : PT\n" \
+"Puerto Rico                                : PR\n"
+
+#define COUNTRY_INIT_Q "Qatar                                      : QA\n"
+
+#define COUNTRY_INIT_R "Republic of Serbia                         : RS\n" \
+"Reunion                                    : RE\n" \
+"Romania                                    : RO\n" \
+"Russia Federation                          : RU\n" \
+"Rwanda                                     : RW\n"
+
+#define COUNTRY_INIT_S "Saint Barthélemy                           : BL\n" \
+"Saint Helena                               : SH\n" \
+"Saint Kitts & Nevis                        : KN\n" \
+"Saint Lucia                                : LC\n" \
+"Saint Martin                               : MF\n" \
+"Saint Pierre and Miquelon                  : PM\n" \
+"Saint Vincent and the Grenadines           : VC\n" \
+"Samoa                                      : WS\n" \
+"San Marino                                 : SM\n" \
+"Sao Tome and Principe                      : ST\n" \
+"Saudi Arabia                               : SA\n" \
+"Senegal                                    : SN\n" \
+"Serbia and Montenegro                      : CS\n" \
+"Seychelles                                 : SC\n" \
+"Sierra Leone                               : SL\n" \
+"Singapore                                  : SG\n" \
+"Sint Maarten                               : SX\n" \
+"Slovakia                                   : SK\n" \
+"Slovenia                                   : SI\n" \
+"Solomon Islands                            : SB\n" \
+"Somalia                                    : SO\n" \
+"South Africa                               : ZA\n" \
+"South Georgia & The South Sandwich Islands : GS\n" \
+"South Sudan                                : SS\n" \
+"Spain                                      : ES\n" \
+"Sri Lanka                                  : LK\n" \
+"Sudan                                      : SD\n" \
+"Suriname                                   : SR\n" \
+"Svalbard and Jan Mayen                     : SJ\n" \
+"Swaziland                                  : SZ\n" \
+"Sweden                                     : SE\n" \
+"Switzerland                                : CH\n" \
+"Syrian Arab Republic                       : SY\n"
+
+#define COUNTRY_INIT_T "Taiwan, Province of China                  : TW\n" \
+"Tajikistan                                 : TJ\n" \
+"Tanzania, United Republic of               : TZ\n" \
+"Thailand                                   : TH\n" \
+"Timor-Leste                                : TL\n" \
+"Togo                                       : TG\n" \
+"Tokelau                                    : TK\n" \
+"Tonga                                      : TO\n" \
+"Trinidad and Tobago                        : TT\n" \
+"Tunisia                                    : TN\n" \
+"Turkey                                     : TR\n" \
+"Turkish Rep N Cyprus (temporary code)      : XT\n" \
+"Turkmenistan                               : TM\n" \
+"Turks and Caicos Islands                   : TC\n" \
+"Tuvalu                                     : TV\n"
+
+#define COUNTRY_INIT_U "Uganda                                     : UG\n" \
+"Ukraine                                    : UA\n" \
+"United Arab Emirates                       : AE\n" \
+"United Kingdom                             : GB\n" \
+"United States                              : US\n" \
+"United States Minor Outlying Islands       : UM\n" \
+"Uruguay                                    : UY\n" \
+"Uzbekistan                                 : UZ\n"
+
+#define COUNTRY_INIT_V "Vanuatu                                    : VU\n" \
+"Venezuela                                  : VE\n" \
+"Vietnam                                    : VN\n" \
+"Virgin Islands, British                    : VG\n" \
+"Virgin Islands, U.S.                       : VI\n"
+
+#define COUNTRY_INIT_W "Wallis and Futuna                          : WF\n" \
+"Western Sahara                             : EH\n"
+
+#define COUNTRY_INIT_X "-"
+
+#define COUNTRY_INIT_Y "Yemen                                      : YE\n"
+
+#define COUNTRY_INIT_Z "Zambia                                     : ZM\n" \
+"Zimbabwe                                   : ZW\n"
